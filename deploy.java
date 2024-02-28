97558cb0af4eb51517b3c8d34f6732cca57e94ef
Audrey Tchamda <audreygentiana@gmail.com>
2024-02-28 01:36:55 -0500

    merging both stage and main updates

--- deploy.java
+++ deploy.java
@@ -1,1 +1,3 @@
-creating a file in dev branch not in master for testing purposes
+creating a file in dev branch not in master for testing purpose
+adding new lines in this file for the stage branch
+want to create a conflict between the stage branch and the main branch
